ba831e8439831c0e8b804cb084f3e77101fa2be6
Igor Druzhchenko <igor_id@ukr.net>
2015-12-09 18:46:45 +0200

    js задание 1

--- GoIT/js/home/lesson10/js/task10.js
+++ GoIT/js/home/lesson10/js/task10.js
@@ -1,20 +1,34 @@
-function pow(num, exp) {
-    if ( (num == 0) && ( exp != 0 ) ) {
-        return 0;
-    }
-    else {
-        if ( (exp == Math.ceil(exp) ) && (exp > 0) ) {
-            result = 1;
-            for (var i = 1; i <= exp; i++) {
-                result *= num
+var myMath = {};
+    myMath.powMessage = '';
+
+    myMath.pow = function(num, exp) {
+        this.powMessage = '';
+        if ( (num === 0) && ( exp != 0 ) ) {
+            return 0;
+        }
+        else {
+            if ( (exp == Math.ceil(exp) ) && (exp > 0) ) {
+                var result = 1;
+                for (var i = 1; i <= exp; i++) {
+                    result *= num
+                }
+                return result;
+            } else {
+                this.powMessage = 'Операция невозможна. Введена степень ' + exp + '. Степень возводимого числа должна быть положительной и целочисленной величиной:';
             }
-            return result;
-        } else {
-            return 'Операция невозможна. Введена степень ' + exp + '. Степень возводимого числа должна быть положительной и целочисленной величиной:';
         }
     }
-}
 
 var num = prompt('Возведение числа в целочисленную положительную степень степень, введите возводимое число: ', '');
 var exp = prompt('введите степень в которую требуется возвести число '+num + ' :');
-console.log('Результат операции: ', pow (num, exp) );
+console.log('Результат операции: ' );
+
+var p = myMath.pow(num, exp);
+
+if ( myMath.powMessage == '' ) {
+    console.log(p);
+} else {
+    console.log(myMath.powMessage);
+}
+
+
